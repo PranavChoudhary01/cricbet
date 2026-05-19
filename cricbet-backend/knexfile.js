67ab3e5e155b3c9f29e0651e483b619adcd38fef
PranavChoudhary01 <tussarjat2@gmail.com>
2026-05-19 13:22:17 +0530

    fix migrations path

--- cricbet-backend/knexfile.js
+++ cricbet-backend/knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
     database: process.env.DB_NAME,
     user:     process.env.DB_USER,
     password: String(process.env.DB_PASSWORD),
+    ssl: { rejectUnauthorized: false }
   },
   migrations: {
     directory: './scripts/migrations'
